b7bea44db9a20f3e137cc5fd94afcc30f18d2f19
mugash70 <cyrilmugash70@gmail.com>
2024-09-29 22:27:20 +0300

    one

--- routes.js
+++ routes.js
@@ -158,10 +158,18 @@ router.route('/:table')
     .delete(async (req, res) => {
         try {
             const { table } = req.params;
-            const { id } = req.query;
+            // const { id } = req.query;
+            const { user_id, monthYear } = req.query;
             const Model = models[table];
             if (!Model) return res.status(404).send('Table not found');
-            const result = await Model.findOneAndDelete({ _id: id });
+            const [month, year] = monthYear.split('-');
+            const startDate = new Date(year, month - 1, 1);
+            const endDate = new Date(year, month, 1);
+            const result = await Model.deleteMany({
+                user_id: user_id,
+                date: { $gte: startDate, $lt: endDate } 
+            });
+            // const result = await Model.findOneAndDelete({ _id: user_id });
             if (!result) return res.status(404).send('Record not found');
             res.status(200).send('Record deleted successfully');
         } catch (error) {
